e93e2d8abea9f83c77ec58b2f6213ea0f3bec496
Diogo Luz <diogoseicaluz@gmail.com>
2021-02-14 12:09:13 +0000

    ♻️ Refactoring

--- backend/src/utils/mail.ts
+++ backend/src/utils/mail.ts
@@ -1,24 +1,24 @@
 import * as nodemailer from 'nodemailer';
 
 export default async function sendEmail(email: string, html: string) {
-    let mailOptions = {
-        from: 'tutoruppt@gmail.com', // O mail da empresa
-        to: email,
-        subject: 'Recuperação de palavra-passe para TutorUp - Estuda ou Ensina !',
-        html
-    };
+  const mailOptions = {
+    from: 'tutoruppt@gmail.com', // O mail da empresa
+    to: email,
+    subject: 'Recuperação de palavra-passe. - TutorUp',
+    html,
+  };
 
-    const transporter = nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
-        port: Number(process.env.MAIL_PORT),
-        auth: {
-            user: process.env.MAIl_USER,
-            pass: process.env.MAIL_PASSWORD
-        },
-        tls: { rejectUnauthorized: false }
-    });
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: Number(process.env.MAIL_PORT),
+    auth: {
+      user: process.env.MAIl_USER,
+      pass: process.env.MAIL_PASSWORD,
+    },
+    tls: { rejectUnauthorized: false },
+  });
 
-    let info: any = await transporter.sendMail(mailOptions);
+  const info: any = await transporter.sendMail(mailOptions);
 
-    return `Message sent: ${info.messageId}`;
+  return `Message sent: ${info.messageId}`;
 }
