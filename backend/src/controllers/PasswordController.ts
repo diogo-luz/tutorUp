38b025cb5435fcfed9fe64c5782f00544d993327
Diogo Luz <diogoseicaluz@gmail.com>
2021-02-14 18:22:51 +0000

    ♻️ Refactoring some code

--- backend/src/controllers/PasswordController.ts
+++ backend/src/controllers/PasswordController.ts
@@ -1,5 +1,3 @@
-import sendMail from '../utils/mail';
-import db from '../database/connection';
 import { Request, Response } from 'express';
 import handlebars from 'handlebars';
 import { v4 as uuidv4 } from 'uuid';
@@ -7,6 +5,8 @@ import { hash } from 'bcryptjs';
 import fs from 'fs';
 import path from 'path';
 import { addHours, isAfter, parseISO } from 'date-fns';
+import db from '../database/connection';
+import sendMail from '../utils/mail';
 
 interface MyRequest extends Request {
   id?: number;
@@ -15,11 +15,13 @@ interface MyRequest extends Request {
 export default class PasswordController {
   async reset(req: MyRequest, res: Response) {
     const { email } = req.body;
-  
-    let userExists = await db('users').where('email', email);
-  
+
+    const userExists = await db('users').where('email', email);
+
     if (userExists.length === 0) {
-      return res.status(400).json({ error: 'The email provided does not exist' });
+      return res
+        .status(400)
+        .json({ error: 'The email provided does not exist' });
     }
 
     const token = uuidv4();
@@ -32,7 +34,7 @@ export default class PasswordController {
       await trx('user_tokens').insert({
         token,
         user_id: id,
-        created_at: new Date()
+        created_at: new Date(),
       });
 
       await trx.commit();
@@ -41,22 +43,28 @@ export default class PasswordController {
         __dirname,
         '..',
         'views',
-        'forgot_password.hbs'
+        'forgot_password.hbs',
       );
 
-      const templateContent = await fs.promises.readFile(forgotPasswordTemplate, {
-        encoding: 'utf-8',
-      });
+      const templateContent = await fs.promises.readFile(
+        forgotPasswordTemplate,
+        {
+          encoding: 'utf-8',
+        },
+      );
 
       const parseTemplate = handlebars.compile(templateContent);
 
-      await sendMail(email, parseTemplate({
-        name,
-        link: `http://localhost:3000/recover-password/${token}`
-      }));
-  
+      await sendMail(
+        email,
+        parseTemplate({
+          name,
+          link: `http://localhost:3000/recover-password/${token}`,
+        }),
+      );
+
       return res.status(201).send();
-    }catch(error) {
+    } catch (error) {
       console.log(error);
 
       await trx.rollback();
@@ -70,26 +78,19 @@ export default class PasswordController {
   async check(req: MyRequest, res: Response) {
     const { token } = req.params;
 
-    const userToken = await db('user_tokens').where(
-      'token',
-      '=',
-      token
-    );
+    const userToken = await db('user_tokens').where('token', '=', token);
 
     if (!userToken[0]) {
-      return res.status(400).json({ message: 'The token doesnt exists'});
+      return res.status(400).json({ message: 'The token doesnt exists' });
     }
 
-    return res.json({message: 'valid token'});
+    return res.json({ message: 'valid token' });
   }
-  
+
   async recover(req: MyRequest, res: Response) {
     const { password, token } = req.body;
 
-    const userToken = await db('user_tokens').where(
-      'token',
-      token
-    );
+    const userToken = await db('user_tokens').where('token', token);
 
     console.log(userToken);
 
@@ -110,11 +111,12 @@ export default class PasswordController {
         .where('id', '=', userExists[0].id)
         .update({ password_hash: hashedPassword });
 
-      await db('user_tokens').where('id','=', userToken[0].id).del();
-      
-      return res.status(204).json({message: 'resource updated successfully'});
-    }else {
-      return res.status(400).json({ message: 'The user or the token, dont exists' });
+      await db('user_tokens').where('id', '=', userToken[0].id).del();
+
+      return res.status(204).json({ message: 'resource updated successfully' });
     }
+    return res
+      .status(400)
+      .json({ message: 'The user or the token, dont exists' });
   }
-};
+}
