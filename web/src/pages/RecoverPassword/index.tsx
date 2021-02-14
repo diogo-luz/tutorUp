b7a021b4f68004a6b386307cdd27b05c723ea6bd
Diogo Luz <diogoseicaluz@gmail.com>
2021-02-14 12:35:15 +0000

    ✨ Add Toasts and validation to password recovery flow

--- web/src/pages/RecoverPassword/index.tsx
+++ web/src/pages/RecoverPassword/index.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useRef } from 'react';
+import * as Yup from 'yup';
 import { useHistory, Link, useParams } from 'react-router-dom';
 import { FormHandles, SubmitHandler } from '@unform/core';
 import { Form } from '@unform/web';
 import Input from '../../components/InputFloatLabel';
+
+import { useToast } from '../../hooks/toast';
 
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 import { ResetPassPage, LogoContent, ResetPassContent } from './styles';
 
 import api from '../../services/api';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 interface RouteParams {
   token: string;
@@ -24,6 +28,8 @@ const RecoverPassword: React.FC = () => {
   const history = useHistory();
   const params = useParams<RouteParams>();
 
+  const { addToast } = useToast();
+
   useEffect(() => {
     api
       .get(`/recover-password/${params.token}`)
@@ -38,12 +44,49 @@ const RecoverPassword: React.FC = () => {
   });
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const handleSubmit: SubmitHandler<FormData> = data => {
-    api.patch('/recover-password', {
-      token: params.token,
-      password: data.password,
-    });
-    history.push('/');
+  const handleSubmit: SubmitHandler<FormData> = async data => {
+    try {
+      // Remove all previous errors
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        password: Yup.string()
+          .min(6, 'No mínimo 6 caracteres')
+          .required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+
+      api.patch('/recover-password', {
+        token: params.token,
+        password: data.password,
+      });
+
+      // disparar um toast
+      addToast({
+        type: 'success',
+        title: 'Senha alterada com sucesso',
+        description: 'Já pode começar a utilizar a sua conta com a nova senha',
+      });
+
+      history.push('/');
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      // disparar um toast
+      addToast({
+        type: 'error',
+        title: 'Erro na recuperação',
+        description:
+          'Ocorreu um erro ao efetuar a recuperação da sua conta, tente novamente.',
+      });
+    }
   };
 
   return (
@@ -64,8 +107,11 @@ const RecoverPassword: React.FC = () => {
       <ResetPassContent>
         <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>
-            Recuperar a sua senha
-            <span>Já esta quase para recuperar !</span>
+            Redefinição de senha
+            <span>
+              Agora só tem de criar uma nova senha e poderá aceder novamente à
+              sua conta.
+            </span>
           </h1>
 
           <div className="inputs">
--- web/src/pages/ResetPassword/index.tsx
+++ web/src/pages/ResetPassword/index.tsx
@@ -1,14 +1,18 @@
 import React, { useRef } from 'react';
+import * as Yup from 'yup';
 import { useHistory, Link } from 'react-router-dom';
 import { FormHandles, SubmitHandler } from '@unform/core';
 import { Form } from '@unform/web';
 import Input from '../../components/InputFloatLabel';
+
+import { useToast } from '../../hooks/toast';
 
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
 import { ResetPassPage, LogoContent, ResetPassContent } from './styles';
 import api from '../../services/api';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 interface FormData {
   name: string;
@@ -19,10 +23,43 @@ const ResetPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const history = useHistory();
 
+  const { addToast } = useToast();
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const handleSubmit: SubmitHandler<FormData> = data => {
-    api.post(`/reset-password`, { email: data.email });
-    history.push('/reset-password-success');
+  const handleSubmit: SubmitHandler<FormData> = async data => {
+    try {
+      // Remove all previous errors
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('Introduza um endereço de email válido')
+          .required('E-mail obrigatório'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+
+      // validation passed
+      api.post(`/reset-password`, { email: data.email });
+
+      history.push('/reset-password-success');
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      // disparar um toast
+      addToast({
+        type: 'error',
+        title: 'Erro na recuperação',
+        description:
+          'Ocorreu um erro ao efetuar a recuperação da sua conta, colocou um email válido?',
+      });
+    }
   };
 
   return (
